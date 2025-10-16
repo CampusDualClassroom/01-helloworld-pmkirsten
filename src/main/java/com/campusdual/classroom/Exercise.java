3142d108a970f8d8dbc578a352f0c181dfcf2362
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2025-10-16 10:22:13 +0200

    Solved Exercise.java

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -3,5 +3,6 @@ package com.campusdual.classroom;
 public class Exercise {
     public static void main(String[] args) {
         //TODO → Write: System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
     }
 }
